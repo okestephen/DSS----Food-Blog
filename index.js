2457a258e2b9ac1d07608d28ea0d1012dc1bdb76
okestephen <okestephen01@gmail.com>
2025-04-15 10:55:10 +0100

    Created ejs files and layout files

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 //Required Modules
-const express = require('express');
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+// Create environement and set Port
 const app = express();
-const fs = require('fs');
-var bodyParser = require('body-parser')
-const {log} = require('console')
-
-
 const PORT = 3000;
 
-app.use(express.static(__dirname + "/public"));
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"))
 
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
